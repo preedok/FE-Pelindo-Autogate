5916ad20e5e84b04099e1d1e7f54dadfc5424b4f
preedok <edo.vivoy21@gmail.com>
2024-12-11 20:16:30 +0700

    fix : confirm

--- src/views/admin/confirm-bc-ca/index.jsx
+++ src/views/admin/confirm-bc-ca/index.jsx
@@ -6,10 +6,11 @@ const ConfirmBcCa = () => {
         <section className="p-6 mx-5 mt-[78px] rounded-lg w-full">
             <Breadcrombss menu={"Confirm BC CA"} submenu={"Confirm BC CA"} />
             <ContentCard>
-                hehed
+                hehedd
             </ContentCard>
         </section>
     )
 }
 
 export default ConfirmBcCa
+confirm
