8ffbcc6d5f1352efcf84220f39131fc5b4274ef0
preedok <edo.vivoy21@gmail.com>
2024-12-11 20:29:18 +0700

    fix : build

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'date-fns': 'date-fns', // Ensure Vite resolves 'date-fns'
-      '@mui/x-date-pickers': '@mui/x-date-pickers', // Ensure Vite resolves '@mui/x-date-pickers'
+      'date-fns': 'date-fns',
+      '@mui/x-date-pickers': '@mui/x-date-pickers',
     },
   },
 });
